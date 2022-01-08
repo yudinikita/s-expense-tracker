480f5c86ed50db575298f369eeaf31865ef3cd29
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-08 19:19:53 +0300

    move main code to function

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,49 @@
-const featuresLottieFirstContainer = document.querySelector(
-  "#features__lottie-first"
-);
-const featuresLottieSecondContainer = document.querySelector(
-  "#features__lottie-second"
-);
-const featuresLottieThirdContainer = document.querySelector(
-  "#features__lottie-third"
-);
+function loadLottieAnimation() {
+  const featuresLottieFirstContainer = document.querySelector(
+    "#features__lottie-first"
+  );
+  const featuresLottieSecondContainer = document.querySelector(
+    "#features__lottie-second"
+  );
+  const featuresLottieThirdContainer = document.querySelector(
+    "#features__lottie-third"
+  );
 
-const featuresLottieFirst = bodymovin.loadAnimation({
-  wrapper: featuresLottieFirstContainer,
-  animType: "svg",
-  loop: true,
-  path: "./animations/first.json",
-  rendererSettings: {
-    progressiveLoad: true,
-  },
-});
+  const featuresLottieFirst = bodymovin.loadAnimation({
+    wrapper: featuresLottieFirstContainer,
+    animType: "svg",
+    loop: true,
+    path: "./animations/first.json",
+    rendererSettings: {
+      progressiveLoad: true,
+    },
+  });
 
-const featuresLottieSecond = bodymovin.loadAnimation({
-  wrapper: featuresLottieSecondContainer,
-  animType: "svg",
-  loop: true,
-  path: "./animations/second.json",
-  rendererSettings: {
-    progressiveLoad: true,
-  },
-});
+  const featuresLottieSecond = bodymovin.loadAnimation({
+    wrapper: featuresLottieSecondContainer,
+    animType: "svg",
+    loop: true,
+    path: "./animations/second.json",
+    rendererSettings: {
+      progressiveLoad: true,
+    },
+  });
 
-const featuresLottieThird = bodymovin.loadAnimation({
-  wrapper: featuresLottieThirdContainer,
-  animType: "svg",
-  loop: true,
-  path: "./animations/third.json",
-  rendererSettings: {
-    progressiveLoad: true,
-  },
-});
+  const featuresLottieThird = bodymovin.loadAnimation({
+    wrapper: featuresLottieThirdContainer,
+    animType: "svg",
+    loop: true,
+    path: "./animations/third.json",
+    rendererSettings: {
+      progressiveLoad: true,
+    },
+  });
 
-featuresLottieFirst.setSpeed(0.7);
-featuresLottieSecond.setSpeed(0.7);
-featuresLottieThird.setSpeed(0.7);
+  const SPEED_ANIMATION = 0.7;
 
-lottie.searchAnimations();
+  featuresLottieFirst.setSpeed(SPEED_ANIMATION);
+  featuresLottieSecond.setSpeed(SPEED_ANIMATION);
+  featuresLottieThird.setSpeed(SPEED_ANIMATION);
+}
+
+loadLottieAnimation();
